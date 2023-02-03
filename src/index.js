87d2d1f5f40d6d57bdd09fe3fddf99f91e378708
BrentHickman <brenthickman@ymail.com>
2023-02-03 15:07:28 -0800

    shows other planet ages on solar age submit.

--- src/index.js
+++ src/index.js
@@ -10,6 +10,21 @@ function showSolarAge(myAge){
   myAge.mercuryYears(myAge.earthAge);
   newMercuryAge.innerText =  "You are " + myAge.mercuryAge + " Mercury years old.";
   document.getElementById("showAges").append(newMercuryAge);
+
+  let newVenusAge = document.createElement("li");
+  myAge.venusYears(myAge.earthAge);
+  newVenusAge.innerText =  "You are " + myAge.venusAge + " Venus years old.";
+  document.getElementById("showAges").append(newVenusAge);
+
+  let newMarsAge = document.createElement("li");
+  myAge.marsYears(myAge.earthAge);
+  newMarsAge.innerText =  "You are " + myAge.marsAge + " Mars years old.";
+  document.getElementById("showAges").append(newMarsAge);
+
+  let newJupiterAge = document.createElement("li");
+  myAge.jupiterYears(myAge.earthAge);
+  newJupiterAge.innerText =  "You are " + myAge.jupiterAge + " Jupiter years old.";
+  document.getElementById("showAges").append(newJupiterAge);
 }
 
 // solar age
