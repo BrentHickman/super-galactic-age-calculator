845006fb99ea6a930d48376e96caf2206b7c41e5
BrentHickman <brenthickman@ymail.com>
2023-02-03 14:59:03 -0800

    displays mercury solar age on submit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ function showSolarAge(myAge){
   let newEarthAge = document.createElement("li");
   newEarthAge.innerText =  "You are " + myAge.earthAge + " Earth years old.";
   document.getElementById("showAges").append(newEarthAge);
+
+  let newMercuryAge = document.createElement("li");
+  myAge.mercuryYears(myAge.earthAge);
+  newMercuryAge.innerText =  "You are " + myAge.mercuryAge + " Mercury years old.";
+  document.getElementById("showAges").append(newMercuryAge);
 }
 
 // solar age
