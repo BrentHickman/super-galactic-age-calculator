440a3eb71ca6ee468212333c530a624e66d8e8f1
BrentHickman <brenthickman@ymail.com>
2023-02-03 14:37:08 -0800

    Ui displays submitted earth age

--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
-import Age from './../src/Age.js'
+import Age from './../src/Age.js';
 
+//show results
+function showSolarAge(myAge){
+  let newEarthAge = document.createElement("li");
+  newEarthAge.innerText =  "You are " + myAge.earthAge + " Earth years old.";
+  document.getElementById("showAges").append(newEarthAge);
+}
+
+// solar age
+function currentAgeSubmit(){
+  let myEarthAge = document.getElementById("earthAgeInput").value;
+  let myAge = new Age(myEarthAge);
+  showSolarAge(myAge);
+  return myAge;
+}
+
+
+//listeners
+
+window.addEventListener("load", () => {
+  const solarAgeForm = document.getElementById("solarAge");
+  let myAge;
+  solarAgeForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    myAge = currentAgeSubmit();
+  });
+
+});
